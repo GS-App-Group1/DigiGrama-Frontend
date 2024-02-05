adfead37c0d2686a1a0d651bbfa292ceef62b87b
Themira Chathumina <110608712+ThemiraChathumina@users.noreply.github.com>
2024-02-05 10:53:15 +0530

    knadjawd

--- src/components/UserApply.tsx
+++ src/components/UserApply.tsx
@@ -249,7 +249,7 @@ const FormComponent: React.FC<FormComponentProps> = ({
         // PUT request for updating user request
         await axios.put(
           `${mainAPI.urls.updateRequest}?nic=${nic}&email=${encodeURIComponent(
-            email
+            email.replace(/@/g, "%40")
           )}&address=${encodeURIComponent(
             address
           )}&civil_status=${encodeURIComponent(
